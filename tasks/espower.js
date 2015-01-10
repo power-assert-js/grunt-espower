2e76e5f84ae0cd35f0fb491fee7dc4a130842fd0
Takuto Wada <takuto.wada@gmail.com>
2015-01-11 00:11:02 +0900

    chore(grunt-espower): update copyright year

--- tasks/espower.js
+++ tasks/espower.js
@@ -3,7 +3,7 @@
  * 
  * https://github.com/twada/grunt-espower
  *
- * Copyright (c) 2013-2014 Takuto Wada
+ * Copyright (c) 2013-2015 Takuto Wada
  * Licensed under the MIT license.
  *   https://github.com/twada/grunt-espower/blob/master/LICENSE-MIT
  */
